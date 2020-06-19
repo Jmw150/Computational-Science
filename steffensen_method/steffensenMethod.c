127e87109e2d8ac7f58c2dcb4e94072180548b6e
Jordan <jmw150@hotmail.com>
2020-06-19 10:24:31 -0400

    preparing for actually useful c code

--- steffensen_method/steffensenMethod.c
+++ steffensen_method/steffensenMethod.c
@@ -2,13 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <gmp.h>
+
 double function(double x)
 {
     return x * x;
 }
 
-double steffensenMethod(double(*f)(double), 
-    double seed, double precision, int iterations)
+// minor version of steffensen's method
+double steffensenMethod(double(*f)(double), double seed, double precision, int iterations)
 {
     if(precision == 0)
     {
@@ -34,10 +36,43 @@ double steffensenMethod(double(*f)(double),
 
 }
 
+void gmp_steffensenmethod(mpq_t ret, double(*f)(double), double seed, double precision, int iterations)
+{
+    mpq_t x;
+    /*
+    if(precision == 0)
+    {
+        exit(-1);
+    }
+
+    double h = precision;
+    double x = seed;
+    // legal gnu c
+    double g(double x)
+    {
+        return (f(x + h) - f(x)) / h;
+    }
+
+    int i = iterations;
+
+    while(i > 0)
+    {
+        x -= f(x) / ((float)(g(x)));
+        i -= 1;
+    }
+    */
+    //return x;
+
+}
+
 
 int main()
 {
     printf("%f\n", 
         steffensenMethod(&function, 3, 0.1, 50));
+    mpq_t h;
+    mpq_init(h);
+    gmp_steffensenmethod(h,&function, 3, 0.1, 50);
+    gmp_printf("%#40Qx\n",h);
     return 0;
 }
